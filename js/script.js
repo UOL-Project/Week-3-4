44643f41e1739040fa8b96cdc067dc68489e2ed3
hrxfab <hrxfab@gmail.com>
2015-01-28 19:28:18 +0100

    Update of M and W pages, about and scripts

--- js/script.js
+++ js/script.js
@@ -26,4 +26,101 @@ $(function () {
         shuffle:'true'},
     loop:'true'
     });  
-})  
+}) 
+;
+/* drag and drop functions */
+function allowDrop(ev) {
+    ev.preventDefault();
+}
+function drag(ev) {
+    ev.dataTransfer.setData("text", ev.target.id);
+}
+function drop(ev) {
+    ev.preventDefault();
+    var data = ev.dataTransfer.getData("text");
+    ev.target.appendChild(document.getElementById(data));
+}
+/* Change the shirt type */
+
+function shirtype(){
+switch (document.getElementById("shirtmodel").value){
+    case "short":
+            document.getElementById("imgcolour").src = "images/t-shirt.png";
+            break;
+    case "long":
+            document.getElementById("imgcolour").src = "images/hoodies.png";
+            break;
+}
+}
+
+
+/* change the shirt colour by Joao & Fabien */
+function shirtcolour(){
+
+var type = document.getElementById("shirtmodel").value;
+if (type === "short") {
+    switch (document.getElementById("color").value){
+        case "white":
+            document.getElementById("imgcolour").src = "images/t-shirt.png";
+            break;
+        case "black":
+            document.getElementById("imgcolour").src = "images/t-shirt_black.png";
+            break;
+        case "blue":
+            document.getElementById("imgcolour").src = "images/t-shirt_blue.png";
+            break;
+        case "green":
+            document.getElementById("imgcolour").src = "images/t-shirt_green.png";
+            break;
+        case "red":
+            document.getElementById("imgcolour").src = "images/t-shirt_red.png";
+            break;
+        case "yellow":
+            document.getElementById("imgcolour").src = "images/t-shirt_yellow.png";
+            break;
+        case "pink":
+            document.getElementById("imgcolour").src = "images/t-shirt_pink.png";
+            break;
+        case "orange":
+            document.getElementById("imgcolour").src = "images/t-shirt_orange.png";
+            break;
+        default:
+            document.getElementById("imgcolour").src = "images/t-shirt.png";
+    }
+}
+    else{
+            switch (document.getElementById("color").value){
+        case "white":
+            document.getElementById("imgcolour").src = "images/hoodies.png";
+            break;
+        case "black":
+            document.getElementById("imgcolour").src = "images/hoodies-black.png";
+            break;
+        case "blue":
+            document.getElementById("imgcolour").src = "images/hoodies-blue.png";
+            break;
+        case "green":
+            document.getElementById("imgcolour").src = "images/hoodies-green.png";
+            break;
+        case "red":
+            document.getElementById("imgcolour").src = "images/hoodies-red.png";
+            break;
+        case "yellow":
+            document.getElementById("imgcolour").src = "images/hoodies-yellow.png";
+            break;
+        case "pink":
+            document.getElementById("imgcolour").src = "images/hoodies-pink.png";
+            break;
+        case "orange":
+            document.getElementById("imgcolour").src = "images/hoodies-orange.png";
+            break;
+        default:
+            document.getElementById("imgcolour").src = "images/hoodies.png";
+    }
+        
+}
+function submit(){
+    window.alert("Your request was sucessfully processed");
+}
+
+}
